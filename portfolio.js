289d3300101be7bd6f356c3215d42c61535046c5
Ty <tyler.peterson@utah.edu>
2016-10-07 18:30:45 -0600

    fade in nav bar on scroll

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,7 @@
 ////////////////// Nav Menu  //////////////////
+
+var amountScrolled = 300;
+
 function openNav(){
   document.getElementById("nav").style.height= "100%";
 }
@@ -7,25 +10,36 @@ function closeNav(){
   document.getElementById("nav").style.height = "0%";
 }
 
-////////////////// Back to top button  //////////////////
-var amountScrolled = 300;
-
 $(document).ready(function(){
 
-    //fade in back to top button
-    $(window).scroll(function() {
-        if ( $(window).scrollTop() > amountScrolled ) {
-            $('a.back-to-top').fadeIn('slow');
-        } else {
-            $('a.back-to-top').fadeOut('slow');
-        }
-    });
-
-    //scroll animation back to top
-    $('a.back-to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 700);
-        return false;
-    });
+//fade in Nav bar
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $('.fixed-nav-bar').fadeIn('slow');
+    } else {
+        $('.fixed-nav-bar').fadeOut('slow');
+    }
 });
+
+////////////////// Back to top button  //////////////////
+
+//fade in back to top button
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $('a.back-to-top').fadeIn('slow');
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+    }
+});
+
+//scroll animation back to top
+$('a.back-to-top').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700);
+    return false;
+});
+
+});
+
+////////////////// Three.js header background  //////////////////
