5db17755bf4c086d4e8dff9e38b8c123b7625a8c
Ty <tyler.peterson@utah.edu>
2016-10-09 10:07:56 -0600

    Add expand collapse for projects

--- portfolio.js
+++ portfolio.js
@@ -11,41 +11,53 @@ function closeNav(){
 
 $(document).ready(function(){
 
-//fade in Nav bar
-$(window).scroll(function() {
-    if ( $(window).scrollTop() > amountScrolled ) {
-        $('.fixed-nav-bar').fadeIn('slow');
-    } else {
-        $('.fixed-nav-bar').fadeOut('slow');
-    }
-});
+    //fade in Nav bar
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > amountScrolled ) {
+            $('.fixed-nav-bar').fadeIn('slow');
+        } else {
+            $('.fixed-nav-bar').fadeOut('slow');
+        }
+    });
 
-$("#portfolio").click(function () {
-    $(".portfolio-logos").slideToggle('slow');
-});
+    //////////expand collapse skills and projects
+    // $("#portfolio").click(function () {
+    //     $(".portfolio-logos").slideToggle('slow');
+    // });
 
-$("#skills").click(function () {
-    $(".skill-icons").slideToggle('slow');
-});
+    // $("#skills").click(function () {
+    //     $(".skill-icons").slideToggle('slow');
+    // });
 
-////////////////// Back to top button  //////////////////
+      $(".wrapper").click(function(){
+        $('.btn').toggleClass( 'active');
+        $(".portfolio-logos").slideToggle('slow');
+      });
 
-//fade in back to top button
-$(window).scroll(function() {
-    if ( $(window).scrollTop() > amountScrolled ) {
-        $('a.back-to-top').fadeIn('slow');
-    } else {
-        $('a.back-to-top').fadeOut('slow');
-    }
-});
+      // $(".wrapper1").click(function(){
+      //   $('.btn').toggleClass( 'active');
+      //   $(".portfolio-logos").slideToggle('slow');
+      // });
+    
 
-//scroll animation back to top
-$('a.back-to-top').click(function() {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 700);
-    return false;
-});
+    ////////////////// Back to top button  //////////////////
+
+    //fade in back to top button
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > amountScrolled ) {
+            $('a.back-to-top').fadeIn('slow');
+        } else {
+            $('a.back-to-top').fadeOut('slow');
+        }
+    });
+
+    //scroll animation back to top
+    $('a.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
 
 });
 
