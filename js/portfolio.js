d45d3ad1205bd43b98001cebc514ec1510bf5781
Ty <tyler.peterson@utah.edu>
2016-10-18 18:16:54 -0600

    remove fullscreen mode in mobile

--- js/portfolio.js
+++ js/portfolio.js
@@ -75,39 +75,5 @@ $(document).ready(function(){
         return false;
     });
 
-
-    var requestFullscreen = function (ele) {
-        if (ele.requestFullscreen) {
-            ele.requestFullscreen();
-        } else if (ele.webkitRequestFullscreen) {
-            ele.webkitRequestFullscreen();
-        } else if (ele.mozRequestFullScreen) {
-            ele.mozRequestFullScreen();
-        } else if (ele.msRequestFullscreen) {
-            ele.msRequestFullscreen();
-        } else {
-            console.log('Fullscreen API is not supported.');
-        }
-    };
-
-    var exitFullscreen = function () {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        } else {
-            console.log('Fullscreen API is not supported.');
-        }
-    };
-
-    document.body.addEventListener('touchstart', function(e) {
-        e.preventDefault();
-        requestFullscreen(document.documentElement);
-    });
-
 });
 
